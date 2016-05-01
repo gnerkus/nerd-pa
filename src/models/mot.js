cbbbd9dac3e50a01c22496de6b336588a38a359d
gnerkus <ifeanyioraelosi@gmail.com>
2016-05-01 18:26:50 +0100

    feat(mot): create 'Mot' resource
    - define Mot model
    - define Mot controller (inherits from BaseController)
    - define API routes for Mot resource

---
+++ src/models/mot.js
@@ -0,0 +1,21 @@
+/**
+ * 2016-05-01 14:25:07
+ * Model definition for a Mot item. A Mot is like a blog article but
+ * smaller. It could be compared to a tweet or tumble (Tumblr).
+ * @param  {Object} SEQUELIZE Sequelize object
+ * @param  {Object} DataTypes Data types
+ * @return {Object}           Mot model definition
+ *
+ * Mot Model Schema:
+ * title: varchar(25)
+ */
+export default (SEQUELIZE, DataTypes) => {
+  const MOT = SEQUELIZE.define('Mot', {
+    content: {
+      type: DataTypes.TEXT,
+      field: 'content',
+    },
+  });
+
+  return MOT;
+};
