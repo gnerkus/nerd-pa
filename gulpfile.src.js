2062a767732d03b1ce23e0a7e7b0608605cd048e
gnerkus <ifeanyioraelosi@gmail.com>
2016-05-04 22:09:14 +0100

    chore(code quality): define gulp task to publish coverage file to Codacy
    The command line tool for Codacy appears to be broken. 'gulp-codacy'
    works and is being employed instead.

---
+++ gulpfile.src.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import gulp from 'gulp';
+import codacy from 'gulp-codacy';
+
+gulp.task('codacy', () => {
+  return gulp
+    .src(['./coverage/lcov.info'], { read: false })
+    .pipe(codacy({
+      token: process.env.ECOUTEBOT_CODACY_TOKEN,
+    }));
+});
