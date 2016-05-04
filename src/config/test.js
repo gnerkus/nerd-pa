f194772e42fc09621205a7273123d4f294f78125
Ifeanyi Oraelosi <ifeanyioraelosi@gmail.com>
2016-05-04 22:52:12 +0100

    test(mot): define tests for Mot resource (#2)
    * chore(version control): ignore documentation and test dist directories
    
    * chore(config): define database options in env config
    
    * refactor(models): read model files synchronously
    
    The files are read synchronously in order to export a fully formed 'DB'
    object that contains references to defined models.
    
    TODO: test asynchronous file loading
    
    * chore(code quality): define gulp task to publish coverage file to Codacy
    
    The command line tool for Codacy appears to be broken. 'gulp-codacy'
    works and is being employed instead.
    
    * chore(build): define compile and build scripts for the application.
    
    * test(mot): define model test for Mot resource

--- src/config/test.js
+++ src/config/test.js
@@ -1,6 +1,10 @@
 export default {
   database: {
     url: 'postgres://ubuntu:@127.0.0.1:5432/circle_test',
+    options: {
+      logging: console.log,
+      dialect: 'postgres',
+    },
   },
   port: 1337,
 };
