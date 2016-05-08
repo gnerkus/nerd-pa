cdcaeca6deb174323549b87331fe3fbdbfdb5b8b
gnerkus <ifeanyioraelosi@gmail.com>
2016-05-08 18:09:40 +0100

    refactor(config): export app from express configuration
    - Initialize configuration in express app
    - Sync models in server (no longer in express app)

--- src/config/express.js
+++ src/config/express.js
@@ -1,31 +1,22 @@
-import config from './index';
-import models from './../models';
-import routes from './../routes';
+import config from 'config/index';
+import models from 'models';
+import routes from 'routes';
 
 import express from 'express';
 import bodyParser from 'body-parser';
 
-function start() {
-  const APP = express();
+const APP = express();
 
-  // App-wide variables
-  APP.set('port', config.port || 1337);
+// App-wide variables
+APP.set('port', config.port || 1337);
 
-  // Application middleware
-  APP.use(bodyParser.json());
-  APP.use(bodyParser.urlencoded({
-    extended: true,
-  }));
+// Application middleware
+APP.use(bodyParser.json());
+APP.use(bodyParser.urlencoded({
+  extended: true,
+}));
 
-  // Sync sequelize models.
-  models.SEQUELIZE.sync().then(() => {
-    // Define routes for the application
-    routes(APP);
+// Define routes for the application
+routes(APP);
 
-    APP.listen(APP.get('port'), () => {
-
-    });
-  });
-}
-
-export default { start };
+export { APP };
--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 const config = require(`./${process.env.NODE_ENV}`);
 
 export default config.default;
