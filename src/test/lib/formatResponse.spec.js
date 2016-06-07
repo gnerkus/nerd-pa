7971945e592099a88a742626ae1cc7f3f4d56a72
gnerkus <ifeanyioraelosi@gmail.com>
2016-06-07 12:10:30 +0100

    feat(formatResponse): specify model type in JSON payload
    The formatResponse utility tool now adds the model type to the JSON
    payload. This is required by the JSON API spec.

--- src/test/lib/formatResponse.spec.js
+++ src/test/lib/formatResponse.spec.js
@@ -17,24 +17,50 @@ describe('#formatResponse', () => {
     },
   ];
 
-  it('should create a data field for a single response', () => {
-    LIB.formatResponse(SAMPLE_RES).should.have.all.keys(['data']);
+  const RES_TYPE = 'resources';
+
+  describe('core method', () => {
+    it('should throw error if no resource specified', () => {
+      (function () {
+        LIB.formatResponse();
+      }).should.throw(Error);
+      (function () {
+        LIB.formatResponse();
+      }).should.throw('no resource specified');
+    });
+
+    it('should throw error is no type specified', () => {
+      (function () {
+        LIB.formatResponse(SAMPLE_RES);
+      }).should.throw(Error);
+      (function () {
+        LIB.formatResponse(SAMPLE_RES);
+      }).should.throw('no type specified');
+    });
   });
 
-  it('should return a resource object with the same attributes', () => {
-    (LIB.formatResponse(SAMPLE_RES).data.attributes.content)
-      .should
-      .equal('An update');
+  describe('single resource', () => {
+    it('should create a data field for a single response', () => {
+      LIB.formatResponse(SAMPLE_RES, RES_TYPE).should.have.all.keys(['data']);
+    });
+
+    it('should return a resource object with the same attributes', () => {
+      (LIB.formatResponse(SAMPLE_RES, RES_TYPE).data.attributes.content)
+        .should
+        .equal('An update');
+    });
   });
 
-  it('should have a data field that is an Array', () => {
-    LIB.formatResponse(COM_RES).should.have.all.keys(['data']);
-    LIB.formatResponse(COM_RES).data.should.be.an.instanceof(Array);
-  });
+  describe('complex resource', () => {
+    it('should have a data field that is an Array', () => {
+      LIB.formatResponse(COM_RES, RES_TYPE).should.have.all.keys(['data']);
+      LIB.formatResponse(COM_RES, RES_TYPE).data.should.be.an.instanceof(Array);
+    });
 
-  it('should return a compound object with the correct response', () => {
-    (LIB.formatResponse(COM_RES).data[0].attributes.content)
-      .should
-      .equal('A single update');
+    it('should return a compound object with the correct response', () => {
+      (LIB.formatResponse(COM_RES, RES_TYPE).data[0].attributes.content)
+        .should
+        .equal('A single update');
+    });
   });
 });
