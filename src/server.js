63d991d651c117ac4f28b0b600f6b938c6c0a781
gnerkus <ifeanyioraelosi@gmail.com>
2016-06-11 15:27:28 +0100

    feat(multiplayer): create socket server

--- src/server.js
+++ src/server.js
@@ -10,6 +10,23 @@ const debug = require('debug')('mottr');
 const APP = require('config/express').APP;
 const MODELS = require('models').default;
 
+const HTTP = require('http').Server(APP);
+const IO = require('socket.io')(HTTP);
+
+IO.on('connection', (socket) => {
+  console.log('A user connected.');
+  // socket.broadcast.emit('A new player has joined', socket);
+  socket.on('disconnect', () => {
+    console.log('A user disconnected.');
+  });
+
+  socket.on('new word', (word) => {
+    console.log('A user has submitted a new word.', word);
+    // Send the new word to all players.
+    IO.emit('new word', word);
+  });
+});
+
 (async function () {
   try {
     await MODELS.SEQUELIZE.sync();
@@ -17,7 +34,7 @@ const MODELS = require('models').default;
     console.log(`An error occured when syncing the models: ${err}`);
   }
 
-  APP.listen(APP.get('port'), () => {
+  HTTP.listen(APP.get('port'), () => {
     debug(`Express server listening on port ${APP.get('port')}`);
   });
 })();
